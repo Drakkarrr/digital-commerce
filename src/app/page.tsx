b8666cd19a5536d9087c757c23c1035dcd71ba0b
drakkarrr/resume <drakkar.alpha00@gmail.com>
2024-01-06 19:47:33 +0800

    build landing basic app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
 import MaxWidthWrapper from '@/components/MaxWidthWrapper';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <MaxWidthWrapper>
       <div className='py-20 mx-auto text-center flex flex-col items-center max-w-3xl'>
-        <h1>Test</h1>
+        <h1 className='text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl'>
+          The very best marketplace here on
+          <span className='text-blue-600'> Milky Way!</span>.
+        </h1>
+        <p className='mt-6 text-lg max-w-prose text-muted-foreground'>
+          Welcome to DigitalCommerce. Lorem ipsum dolor sit, amet consectetur
+          adipisicing elit. Odit optio illum non vero maxime. Dolore.
+        </p>
+        <div className='flex flex-col sm:flex-row gap-4 mt-6'>
+          <Link href='/products'>Shop Now</Link>
+        </div>
       </div>
     </MaxWidthWrapper>
   );
