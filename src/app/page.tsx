49eb84eafbf892325a54c6a877ecb340d7e441b6
drakkarrr/resume <drakkar.alpha00@gmail.com>
2024-01-06 19:35:58 +0800

    dix global styling issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import MaxWidthWrapper from '@/components/MaxWidthWrapper';
 export default function Home() {
   return (
     <MaxWidthWrapper>
-      <h1>test</h1>
+      <div className='py-20 mx-auto text-center flex flex-col items-center max-w-3xl'>
+        <h1>Test</h1>
+      </div>
     </MaxWidthWrapper>
   );
 }
