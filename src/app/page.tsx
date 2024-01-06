9a134bedcb447ba832c8b791f8b7cceb6a43a103
drakkarrr/resume <drakkar.alpha00@gmail.com>
2024-01-06 19:27:04 +0800

    layput setup basics styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
+import MaxWidthWrapper from '@/components/MaxWidthWrapper';
 import Image from 'next/image';
 
 export default function Home() {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  );
+  return <MaxWidthWrapper></MaxWidthWrapper>;
 }
