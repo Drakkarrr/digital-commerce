71bb76ed71708438952bc25e7742a7c1bd882ef1
drakkarrr/resume <drakkar.alpha00@gmail.com>
2024-01-08 08:54:29 +0800

    fet: build dynamic dropdown navlinks

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -6,14 +6,24 @@ import NavItem from './NavItem';
 
 const NavItems = () => {
   const [active, setActive] = React.useState<null | number>(null);
+  const isAnyOpen = active !== null;
+
   return (
     <div className='flex gap-4 h-full'>
       {PRODUCT_CATEGORIES.map((category, index) => {
         const handleOpen = () =>
           active === index ? setActive(null) : setActive(index);
-        const isActive = active === index;
+        const isOpen = active === index;
 
-        return <NavItem />;
+        return (
+          <NavItem
+            key={category.value}
+            category={category}
+            handleOpen={handleOpen}
+            isOpen={isOpen}
+            isAnyOpen={isAnyOpen}
+          />
+        );
       })}
     </div>
   );
