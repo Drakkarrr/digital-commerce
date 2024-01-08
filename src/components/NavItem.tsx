ee214bad62159ee285388e831445b1ecfbc5e79c
drakkarrr/resume <drakkar.alpha00@gmail.com>
2024-01-08 09:17:01 +0800

    fetch the image of product render successfully

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -5,6 +5,8 @@ import { Button } from './ui/button';
 import { PRODUCT_CATEGORIES } from '@/config';
 import { ChevronDown } from 'lucide-react';
 import { cn } from '@/lib/utils';
+import Image from 'next/image';
+import Link from 'next/link';
 
 type Category = (typeof PRODUCT_CATEGORIES)[number];
 
@@ -58,7 +60,19 @@ const NavItem: React.FC<NavItemProps> = ({
                     <div
                       key={item.name}
                       className='group relative text-base sm:text-sm'>
-                      <div className='relative aspect-video overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75'></div>
+                      <div className='relative aspect-video overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75'>
+                        <Image
+                          src={item.imageSrc}
+                          alt='Product castegory image'
+                          fill
+                          className='object-cover object-center'
+                        />
+                      </div>
+                      <Link
+                        href={item.href}
+                        className='mt-6 block font-medium text-gray-900'>
+                        {item.name}
+                      </Link>
                     </div>
                   ))}
                 </div>
