58090315337ad36b5a4c83fc947d0d75f2d34966
drakkarrr/resume <drakkar.alpha00@gmail.com>
2024-01-08 08:31:11 +0800

    mapped the product over the navlinks

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,15 +1,19 @@
 'use client';
 
+import { PRODUCT_CATEGORIES } from '@/config';
 import React from 'react';
 
 const NavItems = () => {
   const [active, setActive] = React.useState<null | number>(null);
   return (
     <div className='flex gap-4 h-full'>
-      <p>NavItems</p>
-      <p>NavItems</p>
-      <p>NavItems</p>
-      <p>NavItems</p>
+      {PRODUCT_CATEGORIES.map((category, index) => {
+        const handleOpen = () =>
+          active === index ? setActive(null) : setActive(index);
+        const isActive = active === index;
+
+        return <p key={index}>{category.label}</p>;
+      })}
     </div>
   );
 };
