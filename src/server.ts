8385dda8d55d77cba6ae3fb8f16e60205f4a8827
drakkarrr/resume <drakkar.alpha00@gmail.com>
2024-01-11 09:33:37 +0800

    finished payload config setup on start server admin payload URL

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,14 @@ const app = express();
 const PORT: number = Number(process.env.PORT) || 3000;
 
 const startServer = async () => {
-  const payload = await getPayloadClient();
+  const payload = await getPayloadClient({
+    initOptions: {
+      express: app,
+      onInit: async (cms) => {
+        cms.logger.info(`Admin URL: ${cms.getAdminURL()}`);
+      },
+    },
+  });
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
