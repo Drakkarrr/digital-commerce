e776db79262c33d41c5d24d0f9e6fd7aa7800d03
drakkarrr/resume <drakkar.alpha00@gmail.com>
2024-01-11 09:48:23 +0800

    config: added payload build config for cms

---
+++ src/payload.config.ts
@@ -0,0 +1,29 @@
+import { buildConfig } from 'payload/config';
+
+export default buildConfig({
+  serverURL: process.env.NEXT_PUBLIC_SERVER_URL || '',
+  // collections: [Users, Products, Media, ProductFiles, Orders],
+  collections: [],
+  routes: {
+    admin: '/sell',
+  },
+  admin: {
+    user: 'users',
+    bundler: webpackBundler(),
+    meta: {
+      titleSuffix: '- DigitalHippo',
+      favicon: '/favicon.ico',
+      ogImage: '/thumbnail.jpg',
+    },
+  },
+  rateLimit: {
+    max: 2000,
+  },
+  editor: slateEditor({}),
+  db: mongooseAdapter({
+    url: process.env.MONGODB_URL!,
+  }),
+  typescript: {
+    outputFile: path.resolve(__dirname, 'payload-types.ts'),
+  },
+});
