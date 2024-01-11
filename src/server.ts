1cccde84aa43650129b2303af759397f3ff52151
drakkarrr/resume <drakkar.alpha00@gmail.com>
2024-01-11 09:09:07 +0800

    setup server intry point

--- src/server.ts
+++ src/server.ts
@@ -4,4 +4,14 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const PORT: number = 3000 || Number(process.env.PORT);
+const PORT: number = Number(process.env.PORT) || 3000;
+
+const startServer = async () => {
+  //   const payload = await getPayloadClient();
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
+
+startServer();
