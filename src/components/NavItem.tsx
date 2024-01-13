8fe65ae3be2bd97dfa71ff73c94a636486a131f1
drakkarrr/resume <drakkar.alpha00@gmail.com>
2024-01-13 10:02:40 +0800

    revamped navitem

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,8 +1,7 @@
 'use client';
 
-import React from 'react';
-import { Button } from './ui/button';
 import { PRODUCT_CATEGORIES } from '@/config';
+import { Button } from './ui/button';
 import { ChevronDown } from 'lucide-react';
 import { cn } from '@/lib/utils';
 import Image from 'next/image';
@@ -13,16 +12,18 @@ type Category = (typeof PRODUCT_CATEGORIES)[number];
 interface NavItemProps {
   category: Category;
   handleOpen: () => void;
+  close: () => void;
   isOpen: boolean;
   isAnyOpen: boolean;
 }
 
-const NavItem: React.FC<NavItemProps> = ({
+const NavItem = ({
+  isAnyOpen,
   category,
   handleOpen,
-  isAnyOpen,
+  close,
   isOpen,
-}) => {
+}: NavItemProps) => {
   return (
     <div className='flex'>
       <div className='relative flex items-center'>
@@ -32,20 +33,21 @@ const NavItem: React.FC<NavItemProps> = ({
           variant={isOpen ? 'secondary' : 'ghost'}>
           {category.label}
           <ChevronDown
-            className={cn(
-              `h-4 w-4 transition-all text-muted-foreground ${
-                isOpen && 'transform rotate-180'
-              }`
-            )}
+            className={cn('h-4 w-4 transition-all text-muted-foreground', {
+              '-rotate-180': isOpen,
+            })}
           />
         </Button>
       </div>
-      {isOpen && (
+
+      {isOpen ? (
         <div
+          onClick={() => close()}
           className={cn(
-            `absolute inset-x-0 top-full text-sm text-muted-foreground ${
-              !isAnyOpen && 'animate-in fade-in-10 slide-in-from-top-5'
-            }`
+            'absolute inset-x-0 top-full text-sm text-muted-foreground',
+            {
+              'animate-in fade-in-10 slide-in-from-top-5': !isAnyOpen,
+            }
           )}>
           <div
             className='absolute inset-0 top-1/2 bg-white shadow'
@@ -58,16 +60,18 @@ const NavItem: React.FC<NavItemProps> = ({
                 <div className='col-span-4 col-start-1 grid grid-cols-3 gap-x-8'>
                   {category.featured.map((item) => (
                     <div
+                      onClick={() => close}
                       key={item.name}
                       className='group relative text-base sm:text-sm'>
                       <div className='relative aspect-video overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75'>
                         <Image
                           src={item.imageSrc}
-                          alt='Product castegory image'
+                          alt='product category image'
                           fill
                           className='object-cover object-center'
                         />
                       </div>
+
                       <Link
                         href={item.href}
                         className='mt-6 block font-medium text-gray-900'>
@@ -83,7 +87,7 @@ const NavItem: React.FC<NavItemProps> = ({
             </div>
           </div>
         </div>
-      )}
+      ) : null}
     </div>
   );
 };
