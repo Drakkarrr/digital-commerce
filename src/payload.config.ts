9261eba27672e1ce08066bb1ed5a6952e68f9ab6
drakkarrr/resume <drakkar.alpha00@gmail.com>
2024-01-17 02:54:39 +0800

    payload config added collections

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -4,11 +4,11 @@ import { webpackBundler } from '@payloadcms/bundler-webpack';
 import { mongooseAdapter } from '@payloadcms/db-mongodb';
 import { slateEditor } from '@payloadcms/richtext-slate';
 import path from 'path';
-// import { Users } from './collections/Users'
-// import { Products } from './collections/Products/Products'
-// import { Media } from './collections/Media'
-// import { ProductFiles } from './collections/ProductFile'
-// import { Orders } from './collections/Orders'
+import { Users } from './collections/Users';
+import { Products } from './collections/Products/Products';
+import { Media } from './collections/Media';
+import { ProductFiles } from './collections/ProductFile';
+import { Orders } from './collections/Orders';
 
 dotenv.config({
   path: path.resolve(__dirname, '../.env'),
@@ -16,8 +16,7 @@ dotenv.config({
 
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || '',
-  // collections: [Users, Products, Media, ProductFiles, Orders],
-  collections: [],
+  collections: [Users, Products, Media, ProductFiles, Orders],
   routes: {
     admin: '/sell',
   },
